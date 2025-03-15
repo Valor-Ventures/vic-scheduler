6c4ff8fe727289966e32c0460827a8bbd77b308a
jlvanhulst <jlvanhulst@gmail.com>
2025-03-14 21:01:20 -0400

    v0

---
+++ scheduler.py
@@ -0,0 +1,34 @@
+from apscheduler.schedulers.background import BackgroundScheduler # runs tasks in the background
+import os
+import requests
+from datetime import datetime, timedelta
+from config import config
+
+# allows us to specify a recurring time for execution
+scheduler = BackgroundScheduler()
+
+def callvicapi(url):
+    base_url = os.getenv("VIC_DJANGO_URL")
+    headers = { "Authorization": f"Bearer {os.getenv('VIC_API_BEARER_TOKEN')}" }
+    response = requests.get(base_url + url, headers=headers)
+    return response.json()
+
+@scheduler.scheduled_job('interval', minutes=1)
+def check_email():
+    print(f"checking email")
+    return callvicapi("/api/checkemail")
+
+#sunday - thursday
+@scheduler.scheduled_job('cron', hour=21, minute=0)
+def vic_daily_tasks():
+    print(f"running vic daily tasks")
+    response = callvicapi("/api/daily")
+    config.logger.info(f"/api/daily: {response}")
+@scheduler.scheduled_job('cron', day_of_week='sun,mon,tue,wed,thu', hour=20, minute=0)
+def vic_calendar_tasks():
+    tomorrow = datetime.now() + timedelta(days=1)
+    response = callvicapi("/api/calendar/?date="+tomorrow.strftime("%Y-%m-%d"))
+    config.logger.info(f"/api/calendar: {response}")
+
+if os.getenv("SCHEDULED_TASKS_ENABLED") == 'TRUE':
+    scheduler.start()
