448bcf46069876707a6a1db9ef9831c99f806ccb
jlvanhulst <jlvanhulst@gmail.com>
2025-06-14 13:03:28 -0400

    switch to vic20

--- scheduler.py
+++ scheduler.py
@@ -1,60 +1,66 @@
-from apscheduler.schedulers.background import BackgroundScheduler # runs tasks in the background
-import os
+from apscheduler.schedulers.background import BackgroundScheduler
 import requests
 from datetime import datetime, timedelta
 import time
 from dotenv import load_dotenv
 import sentry_sdk
+import os
+
 load_dotenv()
 
 # allows us to specify a recurring time for execution
 scheduler = BackgroundScheduler()
 
-def callvicapi(url,vic_instance :str = "vicbot"):
+
+def callvicapi(url,vic_instance : str = "vicbot"):
     if vic_instance == "vicbot":
         base_url = os.getenv("VICBOT_DJANGO_URL")
     elif vic_instance == "vic20":
         base_url = os.getenv("VIC20")
-    headers = { "Authorization": f"Bearer {os.getenv('VIC_API_BEARER_TOKEN')}" }
+    headers = {"Authorization": f"Bearer {os.getenv('VIC_API_BEARER_TOKEN')}"}
     response = requests.get(base_url + url, headers=headers)
     return response.json()
 
+
 @scheduler.scheduled_job('interval', minutes=1)
 def check_email():
-    print(f"checking email")
+    print("checking email")
     return callvicapi("/api/checkemail")
+
 
 @scheduler.scheduled_job('interval', hours=1)
 def vic20_sfapi_ping():
     # purely to keep the SF connection alive
     # this is a workaround for the SF connection dropping
-    print(f"vic20 sfapi ping")
+    print("vic20 sfapi ping")
     return callvicapi("/api/startuprunway/ping",vic_instance="vic20")
 
-#sunday - thursday
+
+# sunday - thursday
 @scheduler.scheduled_job('cron', hour=18, minute=0)
 def vic_daily_tasks():
-    print(f"running vic daily tasks")
-    response = callvicapi("/api/daily")
+    print("running vic daily tasks")
+    callvicapi("/api/daily")
+
 
 @scheduler.scheduled_job('cron', day_of_week='fri', hour=18, minute=30)
 def vic20_weekly_tasks():
-    print(f"running vic20 weekly fireflies email tasks")
-    response = callvicapi("/api/fireflies/week",vic_instance="vic20")
-    
+    print("running vic20 weekly fireflies email tasks")
+    callvicapi("/api/fireflies/week",vic_instance="vic20")
+
+
 @scheduler.scheduled_job('cron', day_of_week='sun,mon,tue,wed,thu', hour=20, minute=0)
 def vic_calendar_tasks():
     tomorrow = datetime.now() + timedelta(days=1)
-    response = callvicapi("/api/calendar?date="+tomorrow.strftime("%Y-%m-%d"))
+    callvicapi("/api/calendar?date=" + tomorrow.strftime("%Y-%m-%d"),vic_instance='vic20')
+
 
 if __name__ == "__main__":
     print("Starting scheduler")
     sentry_sdk.init(
-    dsn="https://ab479c2adc8863d82d53ab75150015db@o4508767931793409.ingest.us.sentry.io/4508998594461696",
-    # Add data like request headers and IP for users,
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
-    send_default_pii=True,
-)
+        dsn="https://ab479c2adc8863d82d53ab75150015db@o4508767931793409.ingest.us.sentry.io/4508998594461696",
+        send_default_pii=True,
+    )
     scheduler.start()
     try:
         while True:
